50cfa371808d9cfa70c5bbdfc021b23344b8ebb5
germg <ger.mazzagentile@gmail.com>
2016-07-02 18:23:22 -0300

    implementacion de metodos de la clase Sanguchetto y tests para la misma

--- Sangucheto/src/main/java/tallerweb/sangucheto/modelo/Sanguchetto.java
+++ Sangucheto/src/main/java/tallerweb/sangucheto/modelo/Sanguchetto.java
@@ -7,52 +7,94 @@ public class Sanguchetto {
 
 	private static Sanguchetto instance = new Sanguchetto();
 	private List<Ingrediente> ingredientes = new LinkedList<Ingrediente>();
-	
-	private Sanguchetto(){}
-	
-	public static Sanguchetto getInstance(){
+
+	private Sanguchetto() {
+	}
+
+	public static Sanguchetto getInstance() {
 		return instance;
 	}
-	
+
 	/**
 	 * Elimina todos los ingredientes del sanguchetto.<br>
+	 * 
+	 * @throws Exception
 	 */
-	public void vaciar(){
-		// Implementar
+	public void vaciar() throws Exception {
+
+		if (!ingredientes.isEmpty()) {
+			boolean removidos = ingredientes.removeAll(ingredientes);
+
+			if (!removidos) {
+				throw new Exception("No se pudo varia la lista de ingredientes.");
+			}
+		}
 	}
-	
+
 	/**
 	 * Agrega un ingrediente al carrito.<br>
+	 * 
 	 * @param ingrediente
+	 * @throws Exception
 	 */
-	public void agregarIngrediente(Ingrediente ingrediente){
-		// Implementar
+	public void agregarIngrediente(Ingrediente ingrediente) throws Exception {
+
+		boolean agregado = ingredientes.add(ingrediente);
+
+		if (!agregado) {
+			throw new Exception("No se pudo agregar el ingrediente.");
+		}
 	}
-	
+
 	/**
 	 * Lista todos los ingredientes que forman parte del sanguchetto.<br>
+	 * 
 	 * @return
 	 */
-	public List<Ingrediente> verIngredientes(){
-		// Implementar
-		return null;
+	public List<Ingrediente> verIngredientes() {
+
+		List<Ingrediente> listaIngredientes = new LinkedList<Ingrediente>();
+
+		for (Ingrediente ingrediente : ingredientes) {
+			if (ingrediente.getTipo() == TipoIngrediente.INGREDIENTE) {
+				listaIngredientes.add(ingrediente);
+			}
+		}
+
+		return listaIngredientes;
 	}
-	
+
 	/**
-     * Lista todos los condimentos que forman parte del sanguchetto.<br>
-     * @return
-     */
-    public List<Ingrediente> verCondimentos(){
-        // Implementar
-        return null;
-    }
-	
+	 * Lista todos los condimentos que forman parte del sanguchetto.<br>
+	 * 
+	 * @return
+	 */
+	public List<Ingrediente> verCondimentos() {
+
+		List<Ingrediente> listaCondimentos = new LinkedList<Ingrediente>();
+
+		for (Ingrediente ingrediente : ingredientes) {
+
+			if (ingrediente.getTipo() == TipoIngrediente.CONDIMENTO) {
+				listaCondimentos.add(ingrediente);
+			}
+		}
+
+		return listaCondimentos;
+	}
+
 	/**
 	 * Devuelve el precio total del sanguchetto.<br>
+	 * 
 	 * @return
 	 */
-	public Double getPrecio(){
-		// Implementar
-		return null;
+	public Double getPrecio() {
+		Double precioTotal = 0d;
+
+		for (Ingrediente ingrediente : ingredientes) {
+			precioTotal += ingrediente.getPrecio();
+		}
+
+		return precioTotal;
 	}
 }
--- Sangucheto/src/test/java/tallerweb/sanguchetto/modelo/SanguchettoTest.java
+++ Sangucheto/src/test/java/tallerweb/sanguchetto/modelo/SanguchettoTest.java
@@ -1,31 +1,100 @@
 package tallerweb.sanguchetto.modelo;
 
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import tallerweb.sangucheto.modelo.Ingrediente;
+import tallerweb.sangucheto.modelo.Sanguchetto;
+import tallerweb.sangucheto.modelo.TipoIngrediente;
 
 public class SanguchettoTest {
 
-    @Test
-    public void testVaciar() {
-        // Implementar
-    }
+	Sanguchetto sanguchetto = null;
 
-    @Test
-    public void testAgregarIngrediente() {
-        // Implementar
-    }
+	@Before
+	public void init() {
+		if (sanguchetto == null) {
+			sanguchetto = Sanguchetto.getInstance();
+		}
+	}
+	
+	/**
+	 * Construye un ingrediente para testear
+	 * */
+	private Ingrediente construirIngrediente(String nombre, TipoIngrediente tipoIngrediente, Double precio) {
+		Ingrediente ingrediente = new Ingrediente();
+		ingrediente.setNombre(nombre);
+		ingrediente.setTipo(tipoIngrediente);
+		ingrediente.setPrecio(precio);
+		return ingrediente;
+	}
 
-    @Test
-    public void testVerIngredientes() {
-        // Implementar
-    }
+	@Test
+	public void testVaciar() throws Exception {
+		Double precioFinalEsperado = 0d;
+		sanguchetto.vaciar();
+		Assert.assertEquals("El precio final obtenido no es el esperado.", precioFinalEsperado, sanguchetto.getPrecio());
+	}
 
-    @Test
-    public void testVerCondimentos() {
-        // Implementar
-    }
+	@Test
+	public void testAgregarIngrediente() throws Exception {
+		sanguchetto.vaciar();
+		Double precioFinalEsperado = 2d;
+		sanguchetto.agregarIngrediente(construirIngrediente("Jamon", TipoIngrediente.INGREDIENTE, 2d));
+		Double precioFinalObtenido = sanguchetto.getPrecio();
+		Assert.assertEquals("El precio final esperado, no coincide con el obtenido", precioFinalEsperado, precioFinalObtenido);
+	}
 
-    @Test
-    public void testGetPrecio() {
-        // Implementar
-    }
+	@Test
+	public void testVerIngredientes() throws Exception {
+		Integer cantidadCondimentosEsperada = 2;
+		sanguchetto.vaciar();
+		sanguchetto.agregarIngrediente(construirIngrediente("Jamon", TipoIngrediente.INGREDIENTE, 2d));
+		sanguchetto.agregarIngrediente(construirIngrediente("Queso", TipoIngrediente.INGREDIENTE, 2d));
+		sanguchetto.agregarIngrediente(construirIngrediente("Mayonesa", TipoIngrediente.CONDIMENTO, 1.5d));
+		sanguchetto.agregarIngrediente(construirIngrediente("Ketchup", TipoIngrediente.CONDIMENTO, 1.5d));
+		sanguchetto.agregarIngrediente(construirIngrediente("Mostaza", TipoIngrediente.CONDIMENTO, 1.5d));
+		
+		List<Ingrediente> listaIngredientes = sanguchetto.verIngredientes();
+		
+		Assert.assertEquals("La cantidad de condimentos obtenida no coincide con la espedada.", cantidadCondimentosEsperada, (Integer)listaIngredientes.size());
+		
+		for(Ingrediente ingrediente : listaIngredientes){
+			Assert.assertEquals("El tipo de ingrediente del ingrediente no es el correcto.", TipoIngrediente.INGREDIENTE, ingrediente.getTipo());
+		}
+	}
+
+	@Test
+	public void testVerCondimentos() throws Exception {
+		Integer cantidadCondimentosEsperada = 3;
+		sanguchetto.vaciar();
+		sanguchetto.agregarIngrediente(construirIngrediente("Jamon", TipoIngrediente.INGREDIENTE, 2d));
+		sanguchetto.agregarIngrediente(construirIngrediente("Queso", TipoIngrediente.INGREDIENTE, 2d));
+		sanguchetto.agregarIngrediente(construirIngrediente("Mayonesa", TipoIngrediente.CONDIMENTO, 1.5d));
+		sanguchetto.agregarIngrediente(construirIngrediente("Ketchup", TipoIngrediente.CONDIMENTO, 1.5d));
+		sanguchetto.agregarIngrediente(construirIngrediente("Mostaza", TipoIngrediente.CONDIMENTO, 1.5d));
+		
+		List<Ingrediente> listaCondimentos = sanguchetto.verCondimentos();
+		
+		Assert.assertEquals("La cantidad de condimentos obtenida no coincide con la espedada.", cantidadCondimentosEsperada, (Integer)listaCondimentos.size());
+		
+		for(Ingrediente ingrediente : listaCondimentos){
+			Assert.assertEquals("El tipo de ingrediente del ingrediente no es el correcto.", TipoIngrediente.CONDIMENTO, ingrediente.getTipo());
+		}
+	}
+
+	@Test
+	public void testGetPrecio() throws Exception {
+		sanguchetto.vaciar();
+		Double precioFinalEsperado = 7.5d;
+		sanguchetto.agregarIngrediente(construirIngrediente("Jamon", TipoIngrediente.INGREDIENTE, 2d));
+		sanguchetto.agregarIngrediente(construirIngrediente("Queso", TipoIngrediente.INGREDIENTE, 2d));
+		sanguchetto.agregarIngrediente(construirIngrediente("Tomate", TipoIngrediente.INGREDIENTE, 2d));
+		sanguchetto.agregarIngrediente(construirIngrediente("Lechuga", TipoIngrediente.INGREDIENTE, 1.5d));
+		Double precioFinalObtenido = sanguchetto.getPrecio();
+		Assert.assertEquals("El precio final esperado, no coincide con el obtenido", precioFinalEsperado, precioFinalObtenido);
+	}
 }
